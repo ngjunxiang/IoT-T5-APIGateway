575bcd00a99a4ef23f9376270260598daf4f2192
Derrick Lee <derrickleemy@gmail.com>
2019-03-07 22:23:40 +0800

    Added routes to web for testing

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', 'MainController@index')->name('index');
+Route::prefix('liveimage')->group(function () {
+    Route::post('/store', 'ApiController@store')->name('store');
+    Route::get('/', 'ApiController@index')->name('index');
+});
