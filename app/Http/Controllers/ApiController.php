f7043c5e31fd6a3941605e2b7c328f34a37f0e9e
Derrick Lee <derrickleemy@gmail.com>
2019-03-19 22:46:40 +0800

    Updated APIController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -20,22 +20,62 @@ class ApiController extends Controller
             if ($request->imageBlob) {
                 $client = new Client();
 
-                $response = $client->post(env('LIVEIMAGE_HOST') . '/api/liveimage/store', [
-                    'form_params' => [
-                        'imageBlob' => $request->imageBlob,
-                    ],
-                ]);
+                $imageName = $this->sendForDecodeAndStorage($client, $request->imageBlob);
+                !is_null($imageName) ? $numPeopleDetected = $this->sendForCounting($client, $imageName) : abort(400, 'Bad Request');
+                !is_null($numPeopleDetected) ? $response = $this->sendForUpdate($client, $imageName, $numPeopleDetected) : abort(400, 'Bad Request');
 
-                if ($response->getStatusCode() === 200) {
-                    $decodedResponse = json_decode($response->getBody()->getContents(), true);
-                    return $decodedResponse;
-                }
+                return $response;
             }
         } catch (Exception $e) {
             return response()->json(['success' => false, 'status' => get_class($e), 'message' => $e->getMessage()]);
         }
 
         return response()->json(['success' => false, 'status' => 400, 'message' => "Invalid parameters"]);
+    }
+
+    public function sendForDecodeAndStorage($client, $imageBlob)
+    {
+        $response = $client->post(env('LIVEIMAGE_HOST') . '/api/liveimage/store', [
+            'form_params' => [
+                'imageBlob' => $imageBlob,
+            ],
+        ]);
+
+        if ($response->getStatusCode() === 200) {
+            $decodedResponse = json_decode($response->getBody()->getContents(), true);
+            $imageName = $decodedResponse['imageName'];
+            return $imageName;
+        }
+
+        return null;
+    }
+
+    public function sendForCounting($client, $imageName)
+    {
+        $response = $client->get(env('PEOPLECOUNTER_HOST') . '/api/peoplecounter/' . $imageName . '.jpg');
+        if ($response->getStatusCode() === 200) {
+            $decodedResponse = json_decode($response->getBody()->getContents(), true);
+            $numPeopleDetected = $decodedResponse['totalDetected'];
+            return $numPeopleDetected;
+        }
+
+        return null;
+    }
+
+    public function sendForUpdate($client, $imageName, $numPeopleDetected)
+    {
+        $response = $client->post(env('LIVEIMAGE_HOST') . '/api/liveimage/' . $imageName, [
+            'form_params' => [
+                'numPeopleDetected' => $numPeopleDetected,
+            ],
+        ]);
+
+        if ($response->getStatusCode() === 200) {
+            $decodedResponse = json_decode($response->getBody()->getContents(), true);
+            return $decodedResponse;
+        }
+
+        return null;
     }
 
     /**
