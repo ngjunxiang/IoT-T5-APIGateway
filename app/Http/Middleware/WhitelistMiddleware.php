1054cb9926504340b782e41d31089eadae7e4667
Derrick Lee <derrickleemy@gmail.com>
2019-03-30 14:00:23 +0800

    Whitelisted kangrui IP

--- app/Http/Middleware/WhitelistMiddleware.php
+++ app/Http/Middleware/WhitelistMiddleware.php
@@ -22,10 +22,10 @@ class WhitelistMiddleware
             $callerIP = $_SERVER['REMOTE_ADDR'];
         }
 
-        // Derrick's Home, Derrick's Office, School's Wifi, Kangrui's Office
+        // Derrick's Home, Derrick's Office, School's Wifi, Kangrui's Office, Kangrui's Home
         // For Testing, include ::1 (localhost)
         // $opt_hosts = ['124.197.124.20', '128.106.215.190', '202.161.33.32', '::1', '127.0.0.1'];
-        $opt_hosts = [env('DERRICK_HOME'), env('DERRICK_OFFICE'), '202.161.33.32', '157.120.248.198'];
+        $opt_hosts = [env('DERRICK_HOME'), env('DERRICK_OFFICE'), '202.161.33.32', '157.120.248.198', '58.182.111.80'];
         $imt_hosts = [env('PI_HOST'), env('DASHBOARD_HOST')];
 
         // Only allow API calls from the below hosts
